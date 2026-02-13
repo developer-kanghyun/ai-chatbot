e86257ea8c6470e65bca2d251da7740bb2e9e6b2
kanghyun <dev.kanghyun@gmail.com>
2026-02-13 10:00:49 +0900

    refactor: 테스트용 Redis 설정 오류 및 SQL 초기화 구문 문제 해결
    - application-test.yml의 Redis URL 형식 및 ddl-auto 설정 최적화
    - schema.sql 내 PostgreSQL ENUM 타입 생성 및 구분자(;;) 구문 안정화
    - 테스트 코드에서 Testcontainers 동적 프로퍼티 주입 방식 개선

--- src/test/java/com/example/chatbot/ChatbotApplicationTests.java
+++ src/test/java/com/example/chatbot/ChatbotApplicationTests.java
@@ -30,8 +30,8 @@ class ChatbotApplicationTests {
         registry.add("spring.datasource.url", postgres::getJdbcUrl);
         registry.add("spring.datasource.username", postgres::getUsername);
         registry.add("spring.datasource.password", postgres::getPassword);
-        registry.add("spring.data.redis.host", redis::getHost);
-        registry.add("spring.data.redis.port", () -> String.valueOf(redis.getMappedPort(6379)));
+        registry.add("spring.data.redis.url", () -> "redis://" + redis.getHost() + ":" + redis.getMappedPort(6379));
+        registry.add("spring.jpa.hibernate.ddl-auto", () -> "create-drop");
     }
 
     @Test
